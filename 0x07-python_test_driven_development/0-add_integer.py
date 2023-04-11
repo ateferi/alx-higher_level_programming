ee4f5e6a7cb55c8afa451c77f5e74cf155092bd1
AbebeTeferi Lemma <abebeteferi.teferi@gmail.com>
2023-04-11 05:47:08 +0300

    Delete 0x07-python_test_driven_development directory

--- 0x07-python_test_driven_development/0-add_integer.py
+++
@@ -1,4 +0,0 @@
-def add_integer(a, b=98):
-    if type(a) not in [float, int] or type(b) not in [float, int]:
-        raise TypeError("a and b must be either integers or floats.")
-    return int(a) + int(b)
